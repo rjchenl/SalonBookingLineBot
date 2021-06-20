13ba57422735c039e169fcc32c463861b5e959b8
rjchenl <kf135117@gmail.com>
2021-06-20 16:57:16 +0800

    try startup port

--- app.py
+++ app.py
@@ -678,7 +678,8 @@ if __name__ == "__main__":
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
     arg_parser.add_argument('-p', '--port', type=int, default=int(os.environ.get('PORT', 8000)), help='port')
-    arg_parser.add_argument('-d', '--debug', default='0.0.0.0', help='debug')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    arg_parser.add_argument('--host', default='0.0.0.0', help='host')
     options = arg_parser.parse_args()
 
     # create tmp dir for download content
