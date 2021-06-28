4985d9c068ae1b84ce746fe1d0974f5928fee504
wuwt4y <wuwt4y@gmail.com>
2021-06-28 15:15:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -685,4 +685,4 @@ if __name__ == "__main__":
     # create tmp dir for download content
     make_static_tmp_dir()
 
-    app.run(debug=options.debug, port=443)
+    app.run(debug=options.debug, port=80)
